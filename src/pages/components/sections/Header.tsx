1113b415c735e7f8a0f5d9b116ea49c64f428d4d
Mercad0 <abimaelmercado05@gmail.com>
2023-04-04 02:32:27 +0000

    optimization

--- src/pages/components/sections/Header.tsx
+++ src/pages/components/sections/Header.tsx
@@ -7,12 +7,12 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="w-full md:h-20 h-16 z-50 bg-white fixed shadow-lg">
-      <ul className="flex justify-between items-center h-full w-full md:text-xl text-md font-bold">
-        <li className="w-[55%] hidden md:block">
+      <div className="flex justify-between items-center h-full w-full md:text-xl text-md font-bold">
+        <div className="w-[55%] hidden md:block">
           <Link href="/" className="hover:text-red-500 text-left ml-8">
             Fotomatic
           </Link>
-        </li>
+        </div>
         <div className="space-x-2 flex justify-between items-center">
           <Link href="/" className="hidden md:block hover:text-red-500">
             Product detail
@@ -23,7 +23,7 @@ export default function Header() {
           <Link href="/" className="hidden md:block hover:text-red-500">
             Follow us
           </Link>
-          <li className="md:hidden w-56">
+          <div className="md:hidden w-56">
             <Image
               src="/ic-logo.svg"
               width={40}
@@ -31,14 +31,14 @@ export default function Header() {
               alt="icon"
               priority={true}
             />
-          </li>
+          </div>
           <AiOutlineInstagram className="w-10 h-10 mx-2" />
-          <li className="md:hidden flex">
+          <div className="md:hidden flex">
             <AiFillCamera className="w-10 h-10 mx-2" />
             <BsPeopleFill className="w-10 h-10" />
-          </li>
+          </div>
         </div>
-      </ul>
+      </div>
     </header>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import Header from "./components/sections/Header";
 import Main from "./components/sections/Main";
 import Footer from "./components/sections/Footer";
+import Head from "next/head";
 
 export default function Home() {
   
   return (
     <>
     <div className="flex flex-col h-full max-w-fit w-screen bg-gray-200">
+    <Head>
+        <title>Fotomatic</title>
+        <meta name="description" content="Fotomatic - A place where Camera lens and Filters exist!" />
+        <link rel="icon" href="/images/cameraIco.png" />
+      </Head>
       <Header />
       <Main />
       <Footer />
